cd97db4e1efb33b7b1b4ceeb2dd883082b9e664b
Elton Rego <rego.elton@gmail.com>
2017-09-26 10:49:50 -0700

    Adding the first version of this CLI Application
    Bugs:
    - A brother that logs in after being logged out might not work properly

---
+++ ClubSignIn.java
@@ -0,0 +1,97 @@
+import java.util.Date;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.text.SimpleDateFormat;
+
+public class ClubSignIn{
+	
+	private static class Brother{
+
+		private String name;
+		private Date timeIn, timeOut;
+
+		Brother(String name, Date timeIn){
+			this.name = name;
+			this.timeIn = timeIn;
+		}
+
+		public void setLogoutTime(Date logoutTime){
+			timeOut = logoutTime;
+		}
+
+		public boolean isLoggedOut(){
+			if(timeOut == null){
+				return false;
+			} return true;
+		}
+
+		public String getBrotherName(){
+			return this.name;
+		}
+
+		public Date getLogInTime(){
+			return this.timeIn;
+		}
+
+		public Date getLogOutTime(){
+			return this.timeOut;
+		}
+
+	}
+
+	public static void main(String[] args){
+
+		Scanner user = new Scanner(System.in);
+		System.out.print("Please enter the name of this event: ");
+		String eventName = user.nextLine();
+
+		HashMap<String, Brother> attendanceMap = new HashMap<String, Brother>();
+		while(true){
+			System.out.println("Please enter your name to login\n(or type finalize to end event and print a report)");
+			String enteredText = user.nextLine();
+			if(enteredText.compareTo("finalize") != 0){
+				// check if the name is logged in. If so log out
+				if(attendanceMap.containsKey(enteredText) && attendanceMap.get(enteredText).isLoggedOut()){
+					Date loginTime = new Date();
+					String nameOrRelogIn = enteredText + " came back and";
+					Brother loggedInBrother = new Brother(nameOrRelogIn, loginTime);
+					attendanceMap.put(nameOrRelogIn, loggedInBrother);
+					System.out.println("Thank you, " + enteredText + ", you are logged IN again\n-----------\n");
+				} else if (attendanceMap.containsKey(enteredText) && !attendanceMap.get(enteredText).isLoggedOut()){
+					Date logoutTime = new Date();
+					Brother loggingOutBrother = attendanceMap.get(enteredText);
+					loggingOutBrother.setLogoutTime(logoutTime);
+					attendanceMap.put(enteredText, loggingOutBrother);
+					System.out.println("Thank you, " + enteredText + ", you are now logged OUT\n-----------\n");
+				} else {
+					Date loginTime = new Date();
+					Brother loggedInBrother = new Brother(enteredText, loginTime);
+					attendanceMap.put(enteredText, loggedInBrother);
+					System.out.println("Thank you, " + enteredText + ", you are logged IN\n-----------\n");
+				}
+			} else {
+				System.out.println("Here is your attendance report for: " + eventName);
+				for(String name : attendanceMap.keySet()){
+					Brother brotherToReport = attendanceMap.get(name);
+					if(!brotherToReport.isLoggedOut()){
+						Date logOutTime = new Date();
+						brotherToReport.setLogoutTime(logOutTime);
+					}
+					String outTimeText, inTimeText;
+					SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+					outTimeText = sdf.format(brotherToReport.getLogOutTime());
+					inTimeText = sdf.format(brotherToReport.getLogInTime());
+
+					System.out.println(brotherToReport.getBrotherName() + " was here from " + 
+							inTimeText + " to " + outTimeText);
+				}
+				break;
+			}
+		}
+		System.out.println("\nThank you for using club-sign-in!");
+		System.out.println("Copyright 2017 Elton Christopher Rego.");
+
+	}
+
+
+}
